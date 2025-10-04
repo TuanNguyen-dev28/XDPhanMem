b3c43971b0c51de34a7a17bd7869c1016290328a
TuanNguyen-dev28 <nat281205@gmail.com>
2025-10-05 03:45:27 +0700

    Dự án

---
+++ src/main/java/com/example/e_commerce_fashion/ECommerceFashionApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce_fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceFashionApplication.class, args);
+	}
+
+}
